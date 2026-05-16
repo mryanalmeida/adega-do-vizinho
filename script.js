24f6ffb52f14e008549a24d7696ed6d8a8a2f4de
Marcos Almeida <tubaraogalatico2509@gmai.com>
2026-05-16 03:54:55 -0300

    Catalogo do site 0.6

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ const products = [
             "Mais sabores de gelo disponíveis",
             "Consulte os sabores disponíveis de energetico"
         ],
-        price: 0,
+        price: 35,
         image: "img/Produtos/red-premium.png",
-        available: false
+        available: true
     },
 
     {
@@ -29,9 +29,9 @@ const products = [
             "Mais sabores de gelo disponíveis",
             "Consulte os sabores disponíveis de energetico"
         ],
-        price: 0,
+        price: 28,
         image: "img/Produtos/red-basico.png",
-        available: false
+        available: true
     },
 
     {
@@ -43,7 +43,7 @@ const products = [
             "Consulte os sabores disponíveis de energetico",
             "Consulte os sabores disponíveis do whisky"
         ],
-        price: 35,
+        price: 40,
         image: "img/Produtos/jack-premium.png",
         available: true
     },
@@ -59,7 +59,7 @@ const products = [
         ],
         price: 38,
         image: "img/Produtos/jack-basico.png",
-        available: false
+        available: true
     },
 
     // ==========================================
@@ -75,7 +75,7 @@ const products = [
             "Consulte os sabores disponíveis de energetico",
             "Consulte os sabores disponiveis do gelo"
         ],
-        price: 0,
+        price: 15,
         //variants: [
         //    {
         //        name: "250ml",
@@ -91,7 +91,7 @@ const products = [
         //    },
         //],
         image: "img/Produtos/eternity.png",
-        available: false
+        available: true
     },
 
 
@@ -106,7 +106,7 @@ const products = [
         topics: [
             "Consulte os sabores disponiveis",
         ],
-        price: 0,
+        price: 1,
         //variants: [
         //    {
         //        name: "250ml",
@@ -122,7 +122,7 @@ const products = [
         //    },
         //],
         image: "img/Produtos/gelos.png",
-        available: false
+        available: true
     },
 
     // ==========================================
@@ -161,7 +161,7 @@ const products = [
     // ==========================================
 
 
-    
+
 
     // ==========================================
     // ==========    ENÉRGETICO    ==============
@@ -204,7 +204,7 @@ const products = [
             "Consulte os sabores disponiveis",
             //"Consulte os sabores disponiveis"
         ],
-        price: 0,
+        price: 10,
         //variants: [
         //    {
         //        name: "250ml",
@@ -220,7 +220,7 @@ const products = [
         //    },
         //],
         image: "img/Produtos/baly.png",
-        available: false
+        available: true
     },
 
     {
@@ -243,7 +243,7 @@ const products = [
         //    },
         //],
         image: "img/Produtos/v!be.png",
-        available: false
+        available: true
     },
 
     {
@@ -266,7 +266,7 @@ const products = [
         //    },
         //],
         image: "img/Produtos/monster.png",
-        available: false
+        available: true
     }
 
 ];
@@ -282,13 +282,13 @@ const WHATSAPP_PHONE = "5511917742509";
 // Configure aqui o horário de funcionamento.
 // day: 0 = domingo, 1 = segunda, 2 = terça, 3 = quarta, 4 = quinta, 5 = sexta, 6 = sábado
 const OPENING_HOURS = [
-    { day: 0, open: "11:00", close: "03:00" }, // DOMINGO
-    { day: 1, open: "11:00", close: "00:00" }, // SEGUNDA-FEIRA
-    { day: 2, open: "11:00", close: "00:00" }, // TERÇA-FEIRA
-    { day: 3, open: "11:00", close: "00:00" }, // QUARTA-FEIRA
-    { day: 4, open: "11:00", close: "00:00" }, // QUINTA-FEIRA
-    { day: 5, open: "18:00", close: "03:00" }, // SEXTA-FEIRA
-    { day: 6, open: "11:00", close: "03:00" }  // SÁBADO
+    { day: 0, open: "00:00", close: "00:01" }, // DOMINGO
+    { day: 1, open: "00:00", close: "00:01" }, // SEGUNDA-FEIRA
+    { day: 2, open: "00:00", close: "00:01" }, // TERÇA-FEIRA
+    { day: 3, open: "00:00", close: "00:01" }, // QUARTA-FEIRA
+    { day: 4, open: "00:00", close: "00:01" }, // QUINTA-FEIRA
+    { day: 5, open: "00:00", close: "00:01" }, // SEXTA-FEIRA
+    { day: 6, open: "00:00", close: "00:01" }  // SÁBADO
 ];
 
 
@@ -514,9 +514,18 @@ function renderProducts(category = null) {
         return;
     }
 
-    const filteredProducts = category
-        ? products.filter(product => product.category === category)
-        : products;
+    const filteredProducts = (
+        category
+            ? products.filter(product => product.category === category)
+            : products
+    ).sort((a, b) => {
+
+        const aAvailable = a.available !== false;
+        const bAvailable = b.available !== false;
+
+        // Produtos disponíveis aparecem primeiro
+        return Number(bAvailable) - Number(aAvailable);
+    });
 
     categoryTitle.innerText = category || "Produtos";
 
